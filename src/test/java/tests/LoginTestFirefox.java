c2dbc91b232b9e7d730681adc74e30b6e4fb5be4
andreikulagin <kullagin97@gmail.com>
2023-02-18 16:03:18 +0000

    Add login tests

---
+++ src/test/java/tests/LoginTestFirefox.java
@@ -0,0 +1,49 @@
+package tests;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LoginTestFirefox {
+
+    private WebDriver driver;
+    private String url;
+
+    @BeforeAll
+    public static void setUpClass() {
+        System.setProperty("webdriver.gecko.driver", "/usr/local/maven/geckodriver");
+    }
+
+    @BeforeEach
+    public void setUp() throws IOException {
+        // Load properties from config file
+        Properties props = new Properties();
+        FileInputStream input = new FileInputStream("src/test/resources/login.properties");
+        props.load(input);
+        url = props.getProperty("url");
+
+        // Create Firefox driver
+        driver = new FirefoxDriver();
+    }
+
+    @Test
+    public void testLogin() {
+
+        driver.get(url);
+
+        String expectedUrl = "http://176.36.27.131:8180/#/login";
+        String actualUrl = driver.getCurrentUrl();
+        assertEquals(expectedUrl, actualUrl);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        // Close browser
+        driver.quit();
+    }
+}
