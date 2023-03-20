f24d315104a5d827fa30cb9d340d64322902f399
Andrei Kulagin <kullagin97@gmail.com>
2023-03-20 13:38:25 +0000

    DEV

--- src/main/java/pageobjects/DepartmentsPage.java
+++ src/main/java/pageobjects/DepartmentsPage.java
@@ -4,11 +4,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+
 public class DepartmentsPage{
     private WebDriver driver;
 
     @FindBy(id = "menu-departments")
     private WebElement departmentsMenu;
+
     @FindBy(id = "new-department")
     private WebElement newDepartmentButton;
 
