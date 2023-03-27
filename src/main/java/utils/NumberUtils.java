3bc24af2031ce391f620abab4105df719602ac6d
Andrei Kulagin <kullagin97@gmail.com>
2023-03-27 10:47:25 +0000

    Task 11. Constants, enum and data generation with the help of Java Collections in Java.

---
+++ src/main/java/utils/NumberUtils.java
@@ -0,0 +1,16 @@
+package utils;
+
+import java.util.Random;
+
+public class NumberUtils {
+
+    public static int generateRandomInt(int min, int max){
+        Random rnd = new Random();
+        return rnd.nextInt(max-min+1)+min;
+    }
+
+    public static double generateRandomDouble(double min, double max){
+        Random rnd = new Random();
+        return min+(max-min)*rnd.nextDouble();
+    }
+}
