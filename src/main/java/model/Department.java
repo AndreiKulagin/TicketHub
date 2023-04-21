83aed43182c537d7cb597140a79a7f4a58b7976a
Viktor Chmel <viktor.chmel@algosec.com>
2023-04-21 22:23:56 +0300

    add Department class with Builder pattern

---
+++ src/main/java/model/Department.java
@@ -0,0 +1,148 @@
+package model;
+
+public class Department {
+    private String title;
+    private String website;
+    private String email;
+    private String phone;
+    private String skype;
+    private String country;
+    private String city;
+    private String street;
+    private Integer building;
+    private String zipcode;
+    private Integer roomNumber;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getSkype() {
+        return skype;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public Integer getBuilding() {
+        return building;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public Integer getRoomNumber() {
+        return roomNumber;
+    }
+
+    public Department(String title, String website, String email, String phone, String skype, String country, String city, String street, Integer building, String zipcode, Integer roomNumber) {
+        this.title = title;
+        this.website = website;
+        this.email = email;
+        this.phone = phone;
+        this.skype = skype;
+        this.country = country;
+        this.city = city;
+        this.street = street;
+        this.building = building;
+        this.zipcode = zipcode;
+        this.roomNumber = roomNumber;
+    }
+
+    public static class Builder {
+        private String title;
+        private String website;
+        private String email;
+        private String phone;
+        private String skype;
+        private String country;
+        private String city;
+        private String street;
+        private Integer building;
+        private String zipcode;
+        private Integer roomNumber;
+
+        public Builder() {}
+
+        public Builder setTitle(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public Builder setWebsite(String website) {
+            this.website = website;
+            return this;
+        }
+
+        public Builder setEmail(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public Builder setPhone(String phone) {
+            this.phone = phone;
+            return this;
+        }
+
+        public Builder setSkype(String skype) {
+            this.skype = skype;
+            return this;
+        }
+
+        public Builder setCountry(String country) {
+            this.country = country;
+            return this;
+        }
+
+        public Builder setCity(String city) {
+            this.city = city;
+            return this;
+        }
+
+        public Builder setStreet(String street) {
+            this.street = street;
+            return this;
+        }
+
+        public Builder setBuilding(Integer building) {
+            this.building = building;
+            return this;
+        }
+
+        public Builder setZipcode(String zipcode) {
+            this.zipcode = zipcode;
+            return this;
+        }
+
+        public Builder setRoomNumber(Integer roomNumber) {
+            this.roomNumber = roomNumber;
+            return this;
+        }
+
+        public Department build() {
+            return new Department(title, website, email, phone, skype, country, city, street, building, zipcode, roomNumber);
+        }
+    }
+}
--- src/test/java/tests/CreateNewDepartment.java
+++ src/test/java/tests/CreateNewDepartment.java
@@ -1,5 +1,6 @@
 package tests;
 
+import model.Department;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -10,16 +11,17 @@ public class CreateNewDepartment extends BaseUiTest{
     @Test
     public void testCreateNewDepartment(){
       wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//button[@id='create-new-ticket']")));
-        DepartmentsPage.Builder builder = new DepartmentsPage.Builder(driver)
-                .setTitle("Andrei Kulagin555")
-                .setPhone("1234567890")
-                .setSkype("new_department_skype")
-                .setWebsite("qa.com")
-                .setEmail("kullagin966@gmail.com")
-                .setCountry("England")
-                .setCity("Wigan");
-        DepartmentsPage departmentsPage = builder.build();
-        departmentsPage.createNewDepartment();
+
+      Department department = new Department.Builder()
+              .setTitle("Andrei Kulagin555")
+              .setPhone("1234567890")
+              .setSkype("new_department_skype")
+              .setWebsite("qa.com")
+              .setEmail("kullagin966@gmail.com")
+              .setCountry("England")
+              .setCity("Wigan")
+              .build();
+      new DepartmentsPage(driver).createNewDepartment(department);
 
     }
 }
